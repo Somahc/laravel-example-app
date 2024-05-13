eadecb3ab14df194befec94a693885af7f79c40d
Soma Sekine <a8121151@aoyama.jp>
2024-05-13 18:34:44 +0900

    37

--- database/seeders/TweetsSeeder.php
+++ database/seeders/TweetsSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use App\Models\Tweet;
 
 class TweetsSeeder extends Seeder
 {
@@ -14,10 +15,6 @@ class TweetsSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('tweets')->insert([
-            'content' => Str::random(100),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        Tweet::factory()->count(10)->create();
     }
 }
