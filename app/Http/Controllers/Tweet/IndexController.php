32fe31a32e585fd3a0df5a1ce9851cba6c65993e
Soma Sekine <a8121151@aoyama.jp>
2024-05-13 18:40:14 +0900

    43

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Tweet;
 
 class IndexController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return view('tweet.index', ['name' => 'laravel']);
+        $tweets = Tweet::all();
+        // dd($tweets);
+        return view('tweet.index')
+            ->with('tweets', $tweets);
     }
 }
