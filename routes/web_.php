5dae6c7dfa2ca0fed9b4319ede1463eade5c2579
Soma Sekine <a8121151@aoyama.jp>
2024-05-15 00:17:07 +0900

    chap3 8

--- routes/web_.php
+++ routes/web_.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,12 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Sample
 Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class, 'show']);
 
 Route::get('/sample/{id}', [App\Http\Controllers\Sample\IndexController::class, 'showId']);
 
-// Tweet
 Route::get('/tweet', App\Http\Controllers\Tweet\IndexController::class);
 
 Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
@@ -37,16 +34,3 @@ Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutCont
     ->name('tweet.update.put')->where('tweetId', '[0-9]+');
 
 Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__ . '/auth.php';
