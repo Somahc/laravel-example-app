59dde5f5f49c163f5170334ed98d481fe1a5c7ab
Soma Sekine <a8121151@aoyama.jp>
2024-05-20 16:30:36 +0900

    p227 9 画像系テーブルの作成等

--- database/seeders/TweetsSeeder.php
+++ database/seeders/TweetsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Image;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
@@ -15,6 +16,11 @@ class TweetsSeeder extends Seeder
      */
     public function run(): void
     {
-        Tweet::factory()->count(10)->create();
+        Tweet::factory()->count(10)->create()->each(
+            fn ($tweet) =>
+            Image::factory()->count(4)->create()->each(
+                fn ($image) => $tweet->images()->attach($image->id)
+            )
+        );
     }
 }
