e00681b70905702352b22909def92c7fcc7b6afb
Soma Sekine <a8121151@aoyama.jp>
2024-05-21 23:33:54 +0900

    p234 20 Alpinejs未解決

--- app/Services/TweetService.php
+++ app/Services/TweetService.php
@@ -10,7 +10,7 @@ class TweetService
 {
     public function getTweets()
     {
-        return Tweet::orderBy('created_at', 'DESC')->get();
+        return Tweet::with('images')->orderBy('created_at', 'DESC')->get();
     }
 
     // 自分のtweetかどうかをチェックするメソッド
